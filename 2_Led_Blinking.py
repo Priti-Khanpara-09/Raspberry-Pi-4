380e0d01af088646e28f0926d881070e4312d031
Priti Khanpara <85742940+Priti-Khanpara-09@users.noreply.github.com>
2021-06-15 17:26:35 +0530

    Create 2_Led_Blinking.py

---
+++ 2_Led_Blinking.py
@@ -0,0 +1,12 @@
+# This is Led Blinking Practical
+
+from gpiozero import LED
+from time import sleep
+
+led = LED(17) #deffine GPIO17 pin as LED
+
+while True:
+    led.on() #LED ON
+    sleep(0.5) #0.5s delay
+    led.off() #LED 0FF
+    sleep(0.5) #0.5s delay
