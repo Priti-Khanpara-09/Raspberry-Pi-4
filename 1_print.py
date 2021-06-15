269712cd591117a84dcd9da73bc297b431ccccbe
Priti Khanpara <85742940+Priti-Khanpara-09@users.noreply.github.com>
2021-06-15 14:28:17 +0530

    Create 1_print.py

---
+++ 1_print.py
@@ -0,0 +1,23 @@
+# This is a Print Statement Program
+'''
+print('Hello Students')
+print("I'm student")
+'''
+
+print('''
+i like to learn
+i like blue color.....
+i like sports.....
+....
+.....
+''')
+
+a = 'LED'
+print(a)
+
+a = 10
+a = 20
+b = 20
+c = a + b
+print('c =',c)
+
